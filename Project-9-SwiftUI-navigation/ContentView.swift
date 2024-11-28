dbef0bd844dfeb72bfde0676fd8a0ab4aff206a9
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-28 17:36:35 -0500

    Programmatic navigation with NavigationStack

--- Project-9-SwiftUI-navigation/ContentView.swift
+++ Project-9-SwiftUI-navigation/ContentView.swift
@@ -7,32 +7,25 @@
 
 import SwiftUI
 
-struct Student: Hashable {
-    var id = UUID()
-    var name: String
-    var age: Int
-}
-
 struct ContentView: View {
     
-    var students: [Student] = [
-        Student(name: "Kevin", age: 25),
-        Student(name: "Julio", age: 37)
-    ]
+    @State private var path = [Int]()
     
     var body: some View {
-        NavigationStack {
-            List(students, id: \.id) { i in
-                NavigationLink("Select \(i.name)", value: i)
-            }.navigationDestination(for: Student.self) { student in
-                Text("Name \(student.name)")
-                Text("Age \(student.age)")
+        NavigationStack(path: $path){
+            VStack {
+                Button("Show 32"){
+                    path = [32]
+                }
+                Button("Show 64") {
+                    path.append(64)
+                }
+                Button("Show 32 then 64") {
+                    path = [32, 64]
+                }
             }
-            
-            List(0..<5) { i in
-                NavigationLink("Select \(i)", value: i)
-            }.navigationDestination(for: Int.self) { selection in
-                Text("Selection \(selection)")
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
             }
         }
     }
