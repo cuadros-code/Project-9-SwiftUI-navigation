fd91892c702eac1af5eececb03a77f6eb5c249ab
kevin cuadros <kevindavidcuadros@gmail.com>
2024-11-28 20:22:36 -0500

    How to make a NavigationStack return to its root view programmatically

--- Project-9-SwiftUI-navigation/ContentView.swift
+++ Project-9-SwiftUI-navigation/ContentView.swift
@@ -7,36 +7,38 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+//    @Binding var path: Int
+    @Binding var path: NavigationPath
+
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home"){
+//                    path.removeAll()
+                    path = NavigationPath()
+                }
+            }
+    }
+}
+
 struct ContentView: View {
     
+//    @State private var path = [Int]()
     @State private var path = NavigationPath()
+    
     
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
                 }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
-            .toolbar {
-                Button("Push 556") {
-                    path.append(556)
-                }
-                
-                Button("Push Hello") {
-                    path.append("Hello")
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected string \(selection)")
-            }
+        }
+        .onChange(of: path) { oldValue, newValue in
+            print(newValue)
         }
     }
 }
